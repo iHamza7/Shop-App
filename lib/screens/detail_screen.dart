1f393de2760f8ea890566db45485bc071b340a4a
Hamzx47 <khanazam3324@gmail.com>
2023-02-17 07:05:53 +0500

    lib changed

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetail extends StatelessWidget {
   const ProductDetail({super.key});
@@ -7,6 +10,7 @@ class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productID = ModalRoute.of(context)!.settings.arguments as String;
+    Provider.of<Products>(context);
 
     return Scaffold(
       appBar: AppBar(
