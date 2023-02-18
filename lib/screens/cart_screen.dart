2472220bc6fe9aa920d5ec3681c71d4b925d2bb1
Hamzx47 <khanazam3324@gmail.com>
2023-02-18 07:51:26 +0500

    lib changed

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crt items'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: const [],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
