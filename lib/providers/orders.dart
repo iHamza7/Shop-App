9f01320e5fc1e0a6d29503732809e130ef882045
Hamzx47 <khanazam3324@gmail.com>
2023-02-19 07:26:04 +0500

    lib changed

---
+++ lib/providers/orders.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({
+    required this.id,
+    required this.amount,
+    required this.products,
+    required this.dateTime,
+  });
+}
+
+class Order with ChangeNotifier {
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+}
