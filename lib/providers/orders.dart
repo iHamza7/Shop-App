fb782d16b3d587e15038247d86df1ff5aa9e510c
Hamzx47 <khanazam3324@gmail.com>
2023-02-19 07:36:29 +0500

    lib changed

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -22,4 +22,17 @@ class Order with ChangeNotifier {
   List<OrderItem> get orders {
     return [..._orders];
   }
+
+  void addOrders(List<CartItem> cartProducts, double total) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+    notifyListeners();
+  }
 }
