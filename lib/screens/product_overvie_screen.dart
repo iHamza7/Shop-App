3bb3a31860493c21f4a51cb1934ee1698ca27cbb
Hamzx47 <khanazam3324@gmail.com>
2023-02-23 07:02:30 +0500

    lib changed

--- lib/screens/product_overvie_screen.dart
+++ lib/screens/product_overvie_screen.dart
@@ -48,7 +48,7 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
               const PopupMenuItem(
                 value: FilterOptions.Favorite,
                 child: Text(
-                  'Only Favorties',
+                  'Only Favortes',
                 ),
               ),
               const PopupMenuItem(
