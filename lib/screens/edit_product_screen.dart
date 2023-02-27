9357f02b54c716e58b847838d1f1be8a749b1b9e
Hamzx47 <khanazam3324@gmail.com>
2023-02-27 09:33:17 +0500

    lib changed

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -55,7 +55,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Edit Products here'),
+        title: const Text('Edit Products'),
         actions: [
           IconButton(
             onPressed: _saveForm,
