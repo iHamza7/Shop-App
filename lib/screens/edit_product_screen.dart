94a7373ec3e7efedb06e78c18f5678b81779efd8
Hamzx47 <khanazam3324@gmail.com>
2023-02-27 09:36:40 +0500

    lib changed

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -96,7 +96,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 ),
                 TextFormField(
                   decoration: const InputDecoration(
-                    labelText: 'Price',
+                    labelText: 'Price please',
                     hintText: '0.00',
                   ),
                   textInputAction: TextInputAction.next,
