22103fbe9a770964be8bf4ec624efbadef9a3b57
Hamzx47 <khanazam3324@gmail.com>
2023-03-03 19:23:44 +0500

    lib changed

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -39,6 +39,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
     super.didChangeDependencies();
     if (_isInit) {
       final productID = ModalRoute.of(context)!.settings.arguments as String;
+      Provider.of<Products>(context, listen: false).findById(productID);
     }
     _isInit = false;
   }
