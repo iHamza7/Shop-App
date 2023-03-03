3b2953426040db5d7b5152c8e9921bae761518e8
Hamzx47 <khanazam3324@gmail.com>
2023-03-03 19:43:28 +0500

    lib changed

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -51,8 +51,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
         'title': _editedProduct.title,
         'description': _editedProduct.description,
         'price': _editedProduct.price.toString(),
-        'imageUrl': _editedProduct.imageUrl,
+
+        // 'imageUrl': _editedProduct.imageUrl,
+        'imageUrl': '',
       };
+      _imageUrlController.text = _editedProduct.imageUrl;
     }
     _isInit = false;
   }
@@ -208,7 +211,6 @@ class _EditProductScreenState extends State<EditProductScreen> {
                     ),
                     Expanded(
                       child: TextFormField(
-                        initialValue: _initValues['imageUrl'],
                         decoration: const InputDecoration(
                           labelText: 'Enter Url',
                           hintText: 'URL',
