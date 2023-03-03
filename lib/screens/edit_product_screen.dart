8c0327af9fe9c929d3b0db2d95e8a13583ad81b8
Hamzx47 <khanazam3324@gmail.com>
2023-03-03 19:50:32 +0500

    lilb changed

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -79,7 +79,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
   void _saveForm() {
     _form.currentState?.save();
     _form.currentState?.validate();
-    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    if (_editedProduct.id != null) {
+      //
+    } else {
+      Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    }
     Navigator.of(context).pop();
   }
 
